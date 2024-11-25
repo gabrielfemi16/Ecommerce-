563a857aaf6e3432b4aee4e9f911662fd5a934d1
gabrielfemi <gabrielefevberha@gmail.com>
2024-11-25 14:51:14 -0800

    slide menu , responsiveness

--- javascript/nav.js
+++ javascript/nav.js
@@ -4,4 +4,11 @@ const closeButton = document.querySelector(".close");
 
 menuButton.addEventListener("click", () => {
   slideMenu.style.right = "0px";
+  closeButton.style.right = "-150px"; 
 });
+
+closeButton.addEventListener("click", () => {
+  slideMenu.style.right = "-350px";  
+  closeButton.style.right = "300px";  
+});
+
